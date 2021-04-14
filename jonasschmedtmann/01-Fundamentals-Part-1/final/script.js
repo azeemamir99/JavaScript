feb54eed0039060acc31199145696a68c67107b3
azeemamir99 <azeemamir99@gmail.com>
2021-04-14 11:42:12 +0530

    valuesandvariables

---
+++ jonasschmedtmann/01-Fundamentals-Part-1/final/script.js
@@ -0,0 +1,18 @@
+let js = 'amazing';
+if (js === 'amazing') alert('js is fun');
+console.log(2 + 3)
+
+console.log('amir');
+console.log('32');
+
+let firstname = "azeem";
+console.log(firstname);
+
+let givenname = "haleem";
+console.log(givenname);
+
+let naam = "givenname";
+console.log(naam);
+
+let fullName = "camelCase"
+console.log(fullName);
