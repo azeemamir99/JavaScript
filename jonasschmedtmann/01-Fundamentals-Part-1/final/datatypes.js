5a8c5c9330617beda26f68f26a7b44a52d3299df
azeemamir99 <azeemamir99@gmail.com>
2021-04-15 11:25:13 +0530

    datatypes

---
+++ jonasschmedtmann/01-Fundamentals-Part-1/final/datatypes.js
@@ -0,0 +1,34 @@
+/*
+let t = "tea";
+if (t = "tea"); alert("have a cup of t");
+
+console.log("tea is tasty");
+*/
+
+console.log(false);
+
+let javaScriptisFun = true;
+console.log(javaScriptisFun);
+
+console.log(typeof true);
+console.log(typeof false);
+console.log(typeof javaScriptisFun);
+console.log(typeof 23);
+console.log(typeof azeem);
+console.log(typeof "azeem");
+console.log(typeof 'amir');
+
+javaScriptisFun = "valuechanged"
+console.log(typeof javaScriptisFun)
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1985;
+console.log(typeof year);
+
+console.log(typeof null);
+
+// null = 1987;
+// console.log(typeof null);
