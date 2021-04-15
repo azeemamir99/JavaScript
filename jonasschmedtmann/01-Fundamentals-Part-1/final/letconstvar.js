c66db7334e499cf6f15d1c50ff89171b07458c39
azeemamir99 <azeemamir99@gmail.com>
2021-04-15 15:23:41 +0530

    letconstvar

---
+++ jonasschmedtmann/01-Fundamentals-Part-1/final/letconstvar.js
@@ -0,0 +1,14 @@
+let age = 34;
+age = 35;
+
+const birthyear = 1987;
+// birthyear = 1985;
+
+// const city;
+
+var job = 'developer';
+job = "programer";
+
+// not a good practice
+aliasName = 'amir';
+console.log(aliasName);
--- jonasschmedtmann/01-Fundamentals-Part-1/starter/external.js
+++ jonasschmedtmann/01-Fundamentals-Part-1/starter/external.js
@@ -1,4 +1,4 @@
 let js = 'amazing'
 js = 'boring'
-if (js === 'amazing') alert('java is fun')
+if (js === 'amazing') alert('js is fun')
 console.log(2 + 3)
