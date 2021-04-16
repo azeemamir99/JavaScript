fd31ad891150ea924c97231a1aedf35b3fa7890d
azeemamir99 <azeemamir99@gmail.com>
2021-04-16 15:21:42 +0530

    coding challenge 1

---
+++ jonasschmedtmann/01-Fundamentals-Part-1/final/codingchallenge1.js
@@ -0,0 +1,25 @@
+const marksWeight1 = "78";
+const marksHeight1 = "1.69";
+const johnWeight1 = "92";
+const johnHeight1 = "1.95";
+
+const marksbmi1 = marksWeight1 / marksHeight1 ** 2;
+const johnbmi1 = johnWeight1 / johnHeight1 ** 2;
+console.log(marksbmi1);
+console.log(johnbmi1);
+
+const testData1 = marksbmi1 > johnbmi1;
+console.log(testData1);
+
+const marksWeight2 = "95";
+const marksHeight2 = "1.88";
+const johnWeight2 = "85";
+const johnHeight2 = "1.76";
+
+const marksbmi2 = marksWeight2 / marksHeight2 ** 2;
+const johnbmi2 = johnWeight2 / johnHeight2 ** 2;
+console.log(marksbmi2);
+console.log(johnbmi2);
+
+const testData2 = marksbmi2 > johnbmi2;
+console.log(testData2);
