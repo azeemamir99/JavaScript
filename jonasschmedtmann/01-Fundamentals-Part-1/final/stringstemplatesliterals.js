d53086e8dc3a8b68b923b44c0c0d10203f344dea
azeemamir99 <azeemamir99@gmail.com>
2021-04-16 17:39:34 +0530

    string template literals back-tick

---
+++ jonasschmedtmann/01-Fundamentals-Part-1/final/stringstemplatesliterals.js
@@ -0,0 +1,20 @@
+const firstName = 'azeem';
+const job = 'developer';
+const birthYear = '1985';
+const year = '2021';
+
+const amir = "I'm " + firstName + ', a ' + (year - birthYear) + ' years old ' + job + '!';
+console.log(amir);
+
+const amirNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+console.log(amirNew);
+
+console.log(`Just a regular string example using back-tick...`);
+
+console.log('string with \n\
+multiple \n\
+lines before es6');
+
+console.log(`but now with 
+back-tick we just hit enter
+ and enter multiple lines`);
