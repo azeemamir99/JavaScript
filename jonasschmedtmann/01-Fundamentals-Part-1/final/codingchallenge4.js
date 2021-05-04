49e49fbc7f95b3885758127eaca91ad4c677a4cb
azeemamir99 <azeemamir99@gmail.com>
2021-05-04 15:18:29 +0530

    coding challenge 4

---
+++ jonasschmedtmann/01-Fundamentals-Part-1/final/codingchallenge4.js
@@ -0,0 +1,15 @@
+const data1 = 275;
+const tip1 = data1 >= 50 && data1 <= 300 ? (data1 * 15) / 100 : (data1 * 20) / 100;
+console.log(`The bill was ${data1}, the tip was ${tip1}, and the total value ${data1 + tip1}`);
+
+const data2 = 40;
+const tip2 = data2 >= 50 && data2 <= 300 ? (data2 * 15) / 100 : (data2 * 20) / 100;
+console.log(`The bill was ${data2}, the tip was ${tip2}, and the total value ${data2 + tip2}`);
+
+const data3 = 430;
+const tip3 = data3 >= 50 && data3 <= 300 ? (data3 * 15) / 100 : (data3 * 20) / 100;
+console.log(`The bill was ${data3}, the tip was ${tip3}, and the total value ${data3 + tip3}`);
+
+const data4 = 49;
+const tip4 = data4 >= 50 && data4 <= 300 ? (data4 * 15) / 100 : (data4 * 10) / 100;
+console.log(`The bill was ${data4}, the tip was ${tip4}, and the total value ${data4 + tip4}`);
