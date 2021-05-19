55e366fa312fe0004c750d88e1a65f9f0cf3f20e
azeemamir99 <azeemamir99@gmail.com>
2021-05-19 22:11:49 +0530

    Intro to Objects

---
+++ jonasschmedtmann/3. JavaScript Fundamentals – Part 2/introobjects.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// example of array
+
+const amirArray = [
+    'Azeem',
+    'Amir',
+    2021 - 1987,
+    'engineer',
+    ['Khalid', 'Wasil', 'Shabaaz']
+];
+console.log(amirArray);
+
+// example of object
+
+const amirObject = {
+    firstName: 'Azeem', //firstName is the key / property and Azeem is the value.
+    lastName: 'Amir',
+    age: 2021 - 1987,
+    job: 'engineer',
+    friends: ['Khalid', 'Wasil', 'Shabaaz']
+};
+console.log(amirObject);
